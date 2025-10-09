ec044d73041ad3e3d732a9deef87140e5a08c7cf
DrAndrewCosta <andrewcosta.md@gmail.com>
2025-10-09 03:09:45 -0300

    Improve export module preloading

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins: [react()] });
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/react")) return "react";
+          if (id.includes("node_modules/react-dom")) return "react";
+          if (id.includes("node_modules/html2canvas")) return "html2canvas";
+          if (id.includes("node_modules/jspdf")) return "jspdf";
+        },
+      },
+    },
+  },
+});
